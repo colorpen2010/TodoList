3190d0332de265e400fbe67befcb7ab2e4007840
GmodHunter <sdsad@gmail.com>
2025-11-14 13:21:04 +0200

    version 1

--- Main.py
+++ Main.py
@@ -12,6 +12,16 @@ def row_adder(event,time,date):
     ]
     model.appendRow(row)
 
+def data_change(type):
+    """
+    :param type: в type лежит тип изменяемого объекта, пример: name,x,y.
+    """
+    index=ui_window.event_list.currentIndex()
+    if type=='Событие':
+        model.setItem(index.row(),0,QStandardItem(ui_window.event_name.text()))
+    if type=='Время':
+        model.setItem(index.row(),1,QStandardItem(str(ui_window.timeEdit.value())))
+
 model=QStandardItemModel(0, 3)
 ui_window=Ui_MainWindow()
 app=QApplication()
@@ -21,7 +31,13 @@ ui_window.setupUi(window)
 
 ui_window.event_list.setModel(model)
 
+ui_window.event_name.textChanged.connect(lambda: data_change("Событие"))
+
 model.setHorizontalHeaderLabels(['Событие','Время','Дата'])
+
+ui_window.event_list.setColumnWidth(0,159)
+ui_window.event_list.setColumnWidth(1,159)
+ui_window.event_list.setColumnWidth(2,159)
 
 ui_window.add_button.clicked.connect(lambda: row_adder('Новый год','23:00','31 декабря'))
 window.setWindowIcon(QPixmap('icon.png'))
